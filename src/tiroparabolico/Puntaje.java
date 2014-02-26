e8dd0aae6f88ac2b120706a73b88aad566f4cd46
Jorge Perales <jorgeluis070294@hotmail.com>
2014-02-25 18:57:52 -0600

    Clase puntaje + métodos para guardar y cargar archivo
    Ya agregue los métodos para guardar y cargar archivos solo que no
    entiendo para que es el arreglo de strings arr. Aun no funciona pero
    ahorita viendo bien eso del arr  y modificandolo debe funcionar.

---
+++ src/tiroparabolico/Puntaje.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tiroparabolico;
+
+/**
+ *
+ * @author jorgelp94
+ */
+public class Puntaje {
+    
+}
